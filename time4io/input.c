0b38d0bab17454cc0843db8a35d40cafbcc8f086
McPantsSenior <117841643+McPantsSenior@users.noreply.github.com>
2023-02-25 15:11:05 +0100

    Some structure

--- time4io/input.c
+++ time4io/input.c
@@ -1,6 +1,6 @@
 #include <stdint.h>   /* Declarations of uint_32 and the like */
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
-#include "mipslab.h"  /* Declatations for these labs */
+#include "header.h"  /* Declatations for these labs */
 
 int getsw(void) {
 	int SW = (PORTD >> 8) & 0x000F; // inputs för switches shiftad 8 till höger 
